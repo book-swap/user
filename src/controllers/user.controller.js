da7a5eb027d845f08fef19b922c22a459b615b52
Mihai Voinea <hi@voinea.me>
2019-07-25 16:37:45 +0300

    Added findOne route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const { sanitizeUser } = require("../helpers/user.helper");
 
 // Retrieve my user
 exports.findMe = (req, res) => {
-  res.send(req.user);
+  res.json(req.user);
 };
 
 exports.updateMe = (req, res) => {
@@ -18,8 +18,8 @@ exports.updateMe = (req, res) => {
     { new: true }
   )
     .then(user => {
-      if (!user) res.status(404).send({ message: "Not found" });
-      else res.send(sanitizeUser(user));
+      if (!user) res.status(404).json({ message: "Not found" });
+      else res.json(sanitizeUser(user));
     })
     .catch(error => {
       res.json({ message: "An error occured" });
@@ -30,8 +30,20 @@ exports.updateMe = (req, res) => {
 exports.deleteMe = (req, res) => {
   User.findOneAndDelete({ _id: req.user.id })
     .then(user => {
-      if (!user) res.status(404).send({ message: "Not found" });
-      else res.send(sanitizeUser(user));
+      if (!user) res.status(404).json({ message: "Not found" });
+      else res.json(sanitizeUser(user));
+    })
+    .catch(error => {
+      res.json({ message: "An error occured" });
+      console.log(error);
+    });
+};
+
+exports.findOne = (req, res) => {
+  User.findById(req.params.userId)
+    .then(user => {
+      if (!user) res.status(404).json({ message: "Not found" });
+      else res.json(sanitizeUser(user));
     })
     .catch(error => {
       res.json({ message: "An error occured" });
--- src/router.js
+++ src/router.js
@@ -4,5 +4,6 @@ const userController = require("./controllers/user.controller");
 router.get("/me", userController.findMe);
 router.patch("/me", userController.updateMe);
 router.delete("/me", userController.deleteMe);
+router.get("/:userId", userController.findOne);
 
 module.exports = router;
